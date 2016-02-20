2f5ed4eea61b40657b82935315192430a39edb72
Ryan Barrar <Rbarrar17@gmail.com>
2016-02-20 12:24:11 -0500

    added t to pos't'gresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160219235330) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.string   "user"
     t.string   "text"
