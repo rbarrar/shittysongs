6cb3a8aa1c70c821eaf2827a8c6a0784871dee0f
Geoff Lipszyc <geofflip@gmail.com>
2016-02-22 09:39:27 -0500

    added videos and desriptions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,85 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Video.create(artist_name: 'Rednex', song_name: 'Cotton Eye Joe', description: 'Their mommas must be so proud...', url: 'https://www.youtube.com/embed/VcDy8HEg1QY')
+Video.create(artist_name: 'Maroon 5', song_name: 'Moves Like Jagger', description:
+"This song explains so much. For instance, the Adam Levine Kmart men's clothing
+collection and the Adam Levine 'Eau De WTF?' fragrance. If Mick was dead he'd be
+rolling in his grave.", url: 'https://www.youtube.com/embed/ZQsK9WzDiuM')
+Video.create(artist_name: 'Ke$ha', song_name: 'TiK ToK', description: "For this
+one I don't Really blame Ke$ha as much as Sony and Dr. Luke, after all she was
+practically a kid, but seriously Bianca? THIS is good music?!?!",
+url: "https://www.youtube.com/embed/iP6XpLQM2Cs")
+Video.create(artist_name: 'Rednex', song_name: 'Cotton Eye Joe', description:
+"Why do I get the feeling that the Rednex were sitting around, trippin' balls
+and decided to put this song out before their cat stole their idea?
+Their mommas must be so proud...", url: 'https://www.youtube.com/embed/VcDy8HEg1QY')
+Video.create(artist_name: 'Iggy Azalea', song_name: 'Fancy', description:
+"I started a petition to get Australia to take Iggy Azalea back
+(http://www.australiawhyyoudodis.com) Please help by contributing. No amount is
+too small. We will also accept donations of Fosters' 6 packs, maybe if we get the
+ozzies drunk enough they won't notice when we drop her off.",
+url: "https://www.youtube.com/embed/O-zpOMYRi0w")
+Video.create(artist_name: 'UB40', song_name: 'Red Red Wine', description:
+"When you take a horrible song from a terrible song writer (see Neil Diamond below)
+and make it worse, you have done a very bad thing. If you like this song you're
+encouraging further crimes against humanity and you suck.", url:
+"https://www.youtube.com/embed/zXt56MB-3vc")
+Video.create(artist_name: 'E-40', song_name: 'Choices (Yup)', description:
+'Does this song suck? (Yup)', url: "https://www.youtube.com/embed/bRChz-OYi9o")
+Video.create(artist_name: 'Psy', song_name: 'Gangnam Style', description:
+'Recently North Korea has been threatening an EMP attack with the ability to wipe
+out power grids and bring modern civilisation to a grinding halt. I blame this song
+specifically. Kim, bubbie, I feel you, but maybe put on some Pink Floyd, smoke a
+doobie, and watch Dark Side of the Rainbow, huh?',
+url: "https://www.youtube.com/embed/CH1XGdu-hzQ")
+Video.create(artist_name: 'Corey Feldman', song_name: 'Ascension Millenium',
+description: 'Corey, I was standing by you until I saw this steaming pile of crap.
+You are lost boy, snap out of it!', url: "https://www.youtube.com/embed/hZ67H9JHAHM")
+Video.create(artist_name: 'Black Eyed Peas', song_name: 'My Humps', description:
+"Listen. We all know that no one is listening to Black Eyed Peas for meaningful
+lyrics. You want to grind up on shorty on the dance floor, I get it. Still, it
+is possibe to write beats that don't make people stupider, you know? The lyrics
+sound like they were written by an 8 year old who could've used a spanking or two.
+Just sayin'", url: "https://www.youtube.com/embed/iEe_eraFWWs")
+Video.create(artist_name: 'Black Eyed Peas', song_name: "Boom, Boom, Pow",
+description: "On second thought, I'll take her humps...",
+url: "https://www.youtube.com/embed/4m48GqaOz90")
+Video.create(artist_name: 'LMFAO', song_name: "Sexy and I Know It", description:
+"Sexy? LMFAO!!!", url: "https://www.youtube.com/embed/wyx6JDQCslE")
+Video.create(artist_name: 'Rihanna', song_name: "Umbrella", description:
+"Okay. Domestic violence is never the answer. That being said, after hearing this song,
+I kinda understood where Chris Brown was coming from.",
+url: "https://www.youtube.com/embed/CvBfHwUxHIk")
+Video.create(artist_name: 'Billy Ray Cyrus', song_name: "Achy Breaky Heart",
+description: "Not just did he leave us with this atrocity, but he gave us Miley too.
+FU 2 times Billy Ray!", url: "https://www.youtube.com/embed/byQIPdHMpjc")
+Video.create(artist_name: 'Eddie Murphy', song_name: "Party All The Time",
+description: "What do you get when you put the funniest man on the planet together
+with the King of Punk Funk? Unfortunately this load of crap. Why, Eddie, why?",
+url: "https://www.youtube.com/embed/iWa-6g-TbgI")
+Video.create(artist_name: 'Daniel Powter', song_name: "Bad Day", description:
+"I was having a good day until I heard this one Danny...",
+url: "https://www.youtube.com/embed/gH476CxJxfg")
+Video.create(artist_name: 'Nickelback', song_name: "Rock Star", description:
+"Screw the nickel, I want ALL my money back!", url: "https://www.youtube.com/embed/DmeUuoxyt_E")
+Video.create(artist_name: 'Los del Rio', song_name: "Macarena", description:
+"I'm not even gonna focus on how many times I had to listen to this feculent
+ballad that filled wedding halls and bar mitzvah parties all through the 90's.
+Instead I'll just remind you that it's about a girl having a 3some while her boyfriend
+is getting drafted. 'Nuff said.", url: "https://www.youtube.com/embed/XiBYM6g8Tck")
+Video.create(artist_name: 'Rebecca Black', song_name: "Friday", description: "TGIFU!",
+url: "https://www.youtube.com/embed/kfVsfOSbJY0")
+Video.create(artist_name: 'The Baha Men', song_name: "Who Let The Dogs Out",
+description: "They haven't produced a song since, hopefully one of the dogs got em.",
+url: "https://www.youtube.com/embed/UPI1nAqkpLQ")
+Video.create(artist_name: 'Lou Bega', song_name: "Mombo No. 5", description:
+"Mombo No. Thank You!", url: "https://www.youtube.com/embed/EK_LN3XEcnw")
+Video.create(artist_name: 'Neil Diamond', song_name: "Sweet Caroline", description:
+"If you like this song I'm guessing you're a Red Sox fan. This means you're just now
+downing your 7th $14 stadium beer and screaming along to the 'Oh, oh, oh!' part
+while over-aggressively putting your arm around your buddy's neck. No one likes
+you.", url: "https://www.youtube.com/embed/1vhFnTjia_I")
+Video.create(artist_name: 'Bonus: Lamb-Chop', song_name: "The Song That Never Ends",
+description: "If you've ever had kids and they loved to watch Shari Lewis and Lamb-Chop,
+you know this little ditty. And if you're like me, you made some inquiries about offing
+that cute, little, white bastard.", url: "https://www.youtube.com/embed/b4ztfrr8fls")
